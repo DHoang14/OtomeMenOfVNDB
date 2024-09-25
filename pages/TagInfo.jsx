42096332afb7b9f051f67a4a0bfe9fafc55fbc2f
DHoang14 <dhoang9@asu.edu>
2024-09-24 21:08:53 -0700

    updated links in tags page to be underlined

--- pages/TagInfo.jsx
+++ pages/TagInfo.jsx
@@ -1,19 +1,18 @@
 import React from "react"
 import traitsJson from "../traits.json"
-
+import parse from "html-react-parser"
 
 export default function TagInfo() {
-
     const personalityInfo = traitsJson.personality_traits.map(trait => {
         return (
-            <p key={trait.id}>{trait.name}: {trait.description}</p>
+            <p className="tag-item" key={trait.id}>{trait.name}: {parse(trait.description)}</p>
         )
     })
 
     return (
         <>
-            <div>
-                <h1>Descriptions for Personality Tags</h1>
+            <div className="tag-content">
+                <h1 className="tag-header">Descriptions for Personality Tags</h1>
                 {personalityInfo}
             </div>
         </>
